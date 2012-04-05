3bcbdccd4823ba27cf76541bcf89f149bbb71792
nborwankar <nborwankar@gmail.com>
2012-04-05 16:22:55 -0700

    pushing after user and micropost

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '585ac0ea00938ee352739084663fdbe145f65248beb5882b2440bebc6337a4ad71b20d6bf2941ee591d36da73c010b4eb6872e96154307823e0e6ea7cb4a0dc3'
